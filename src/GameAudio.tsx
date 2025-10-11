94025f32ad033d857073f07b4c5538fcfb94537e
dtinth on MBP M1 <dtinth@spacet.me>
2025-10-11 11:55:06 +0700

    add reverb

--- src/GameAudio.tsx
+++ src/GameAudio.tsx
@@ -5,14 +5,33 @@ export class GameAudio {
   songBufferSource = audioContext.createBufferSource();
   keyBufferSource = audioContext.createBufferSource();
   keyGainNode = audioContext.createGain();
+  reverbNode = audioContext.createConvolver();
   startedAt: number | null = null;
   constructor() {
     this.songBufferSource.connect(this.gainNode);
     this.keyBufferSource.connect(this.keyGainNode);
-    this.gainNode.connect(audioContext.destination);
+    this.keyGainNode.connect(this.reverbNode);
     this.keyGainNode.connect(this.gainNode);
+    this.reverbNode.connect(this.gainNode);
+    this.gainNode.connect(audioContext.destination);
     this.gainNode.gain.value = 0.75;
     this.keyGainNode.gain.value = 0;
+    this.createReverbImpulse();
+  }
+  createReverbImpulse() {
+    const sampleRate = audioContext.sampleRate;
+    const length = sampleRate * 2; // 2 second reverb
+    const impulse = audioContext.createBuffer(2, length, sampleRate);
+    const impulseL = impulse.getChannelData(0);
+    const impulseR = impulse.getChannelData(1);
+
+    for (let i = 0; i < length; i++) {
+      const decay = Math.exp(-i / (sampleRate * 0.5));
+      impulseL[i] = (Math.random() * 2 - 1) * decay;
+      impulseR[i] = (Math.random() * 2 - 1) * decay;
+    }
+
+    this.reverbNode.buffer = impulse;
   }
   play(songAudio: AudioBuffer, keyAudio: AudioBuffer) {
     this.songBufferSource.buffer = songAudio;
